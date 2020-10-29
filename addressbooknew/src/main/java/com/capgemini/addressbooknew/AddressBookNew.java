2512c65d060eacf313163cb8cd12011deb5fd775
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-29 09:29:33 -0700

    [Manasi]Address Book is created

---
+++ addressbooknew/src/main/java/com/capgemini/addressbooknew/AddressBookNew.java
@@ -0,0 +1,13 @@
+package com.capgemini.addressbooknew;
+
+/**
+ * Hello world!
+ *
+ */
+public class AddressBookNew 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ addressbooknew/src/test/java/com/capgemini/addressbooknew/AddressBookNewTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.addressbooknew;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AddressBookNewTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
