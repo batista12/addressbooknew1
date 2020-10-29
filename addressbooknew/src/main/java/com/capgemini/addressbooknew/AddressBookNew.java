f884c4242bafb5e4c3a5e4b670561701e2bff824
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-29 09:51:15 -0700

    [Manasi]UC1 Ability to read write as CSV file

--- addressbooknew/src/main/java/com/capgemini/addressbooknew/AddressBookNew.java
+++ addressbooknew/src/main/java/com/capgemini/addressbooknew/AddressBookNew.java
@@ -1,13 +1,12 @@
 package com.capgemini.addressbooknew;
 
 /**
- * Hello world!
+ * 
  *
  */
 public class AddressBookNew 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
     }
 }
